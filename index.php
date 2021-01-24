7f247d37ab7d117bc22edcc3d5235dd86f537d61
Panagiotis Pantazopoulos <takispadaz@gmail.com>
2021-01-24 16:43:34 +0200

    update example file

--- index.php
+++ index.php
@@ -6,12 +6,10 @@ define("APP_DIR", __DIR__);
 use Biblionet\ApiFetcher;
 
 $fetcher = new ApiFetcher("testuser", "testpsw");
-//$fetcher->fetchById(['251710', '252220']);
-$fetcher->fetch("2021-01");
 
+$fetcher->fetch(ApiFetcher::FETCH_BY_MONTH)->filter('id', 252655, ">")->fill();
 
 $fetchedItems = $fetcher->getFetchedItems();
-
 
 foreach ($fetchedItems as $item){
     echo $item->getTitle() . " => " . $item->getLanguage()->getName().PHP_EOL;
